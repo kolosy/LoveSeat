eb9cb27a29bf0f50e089982b9b965c0b571f4014
kolosy <github@kolosy.com>
2009-09-20 15:42:14 -0500

    added /lib, modified a few ui things, and included a Divan build that fixes a view sync issue.

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -72,7 +72,7 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.rtSource);
-            this.splitContainer1.Size = new System.Drawing.Size(706, 495);
+            this.splitContainer1.Size = new System.Drawing.Size(1164, 796);
             this.splitContainer1.SplitterDistance = 235;
             this.splitContainer1.TabIndex = 0;
             // 
@@ -86,7 +86,7 @@
             this.tvMain.Name = "tvMain";
             this.tvMain.PathSeparator = "/";
             this.tvMain.SelectedImageIndex = 0;
-            this.tvMain.Size = new System.Drawing.Size(235, 495);
+            this.tvMain.Size = new System.Drawing.Size(235, 796);
             this.tvMain.TabIndex = 0;
             this.tvMain.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.tvMain_BeforeExpand);
             this.tvMain.DoubleClick += new System.EventHandler(this.tvMain_DoubleClick);
@@ -149,10 +149,10 @@
             // 
             this.rtSource.AcceptsTab = true;
             this.rtSource.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rtSource.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtSource.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rtSource.Location = new System.Drawing.Point(0, 0);
             this.rtSource.Name = "rtSource";
-            this.rtSource.Size = new System.Drawing.Size(467, 495);
+            this.rtSource.Size = new System.Drawing.Size(925, 796);
             this.rtSource.TabIndex = 0;
             this.rtSource.TabStop = false;
             this.rtSource.Text = "";
@@ -168,7 +168,7 @@
             this.cmdCommit});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(706, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1164, 25);
             this.toolStrip1.Stretch = true;
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
@@ -213,9 +213,9 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 526);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 827);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(706, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(1164, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -229,13 +229,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(706, 548);
+            this.ClientSize = new System.Drawing.Size(1164, 849);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.splitContainer1);
             this.KeyPreview = true;
             this.Name = "FrmMain";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Couch Surfer";
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyUp);
             this.splitContainer1.Panel1.ResumeLayout(false);
