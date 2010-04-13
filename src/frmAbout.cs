3d3c2d8fa8f072f2af3e6c669859c874676406da
kolosy <github@kolosy.com>
2010-04-13 13:39:41 -0500

    added installer

--- src/frmAbout.cs
+++ src/frmAbout.cs
@@ -25,9 +25,9 @@ namespace LoveSeat
         public frmAbout()
         {
             InitializeComponent();
-            this.Text = String.Format("About {0} {0}", AssemblyTitle);
+            this.Text = String.Format("About {0}", AssemblyTitle);
             this.labelProductName.Text = AssemblyProduct;
-            this.labelVersion.Text = String.Format("Version {0} {0}", AssemblyVersion);
+            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
             this.labelCopyright.Text = AssemblyCopyright;
             this.labelCompanyName.Text = AssemblyCompany;
             this.textBoxDescription.Text = AssemblyDescription;
